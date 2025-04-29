c0f358009e658313b116befee6efe1b1392ac387
Oakk <92306399+YKXu-c@users.noreply.github.com>
2025-04-29 10:05:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -71,7 +71,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，here is my Notes of Condensed matter theory and related mathematic🎉Welcome!',
+    'Hi, here is my Notes of Condensed matter theory and related mathematic🎉Welcome!',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
