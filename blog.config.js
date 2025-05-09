a460e5216617ed3a2fb8ff2f3717e64c59d35cfe
Oakk <92306399+YKXu-c@users.noreply.github.com>
2025-05-09 09:01:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Oakk', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'A physical researcher who is intersted in everything.', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'About RustNotes.', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.oakk.space', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'rust, programmer, code, algorithm, 算法, 编程, 代码, 程序员', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
